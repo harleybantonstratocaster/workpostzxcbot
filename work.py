8e987ae2d1df2a377ea391504f5256e740897b72
harleybentonstratocaster <113093959+harleybantonstratocaster@users.noreply.github.com>
2023-07-21 19:26:19 +0300

    Initial commit

---
+++ work.py
@@ -0,0 +1,57 @@
+from telethon import TelegramClient, events
+from datetime import timedelta
+import asyncio
+import random
+
+accounts = [
+    {"api_id": 28300645, "api_hash": '5f25371da2bf53707fdad2cbf4321d44', "session": 'work1', "name": 'Sophia',"listened_phrases": []},
+    {"api_id": 25842680, "api_hash": 'c9b5f4e951ca79f2c061cf9842c95902', "session": 'work2', "name": 'Natalie',"listened_phrases": []},
+]
+
+phrases = ['1', '2', '3']
+
+listen_time = None
+clients = []
+
+
+async def message_handler(event, client, account):
+    global listen_time
+    sender = await event.get_sender()
+    if 'start_reg' in event.raw_text:
+        prev_delay = timedelta(seconds=0)
+        for msg, delay in account["listened_phrases"]:
+            wait_time = delay - prev_delay
+            await asyncio.sleep(wait_time.total_seconds())
+            await client.send_message(event.chat_id, msg)
+            prev_delay = delay
+    if 'start check' in event.raw_text:
+        random_element = random.randint(0, len(phrases) - 1)
+        await client.send_message(event.chat_id, phrases[random_element])
+    if 'forget' in event.raw_text:
+        for account in accounts:
+            account["listened_phrases"].clear()
+    if 'listen' in event.raw_text:
+        listen_time = event.date
+        for account in accounts:
+            account["listened_phrases"].clear()
+    if sender.first_name == account["name"]:
+        if listen_time:
+            message_time = event.date
+            difference = message_time - listen_time
+            account["listened_phrases"].append((event.raw_text, difference))
+
+
+def bind_event_handler(client, account):
+    client.on(events.NewMessage())(lambda event: message_handler(event, client, account))
+
+
+for account in accounts:
+    client = TelegramClient(account['session'], account['api_id'], account['api_hash'])
+    bind_event_handler(client, account)
+    clients.append(client)
+
+for client in clients:
+    client.start()
+
+for client in clients:
+    client.run_until_disconnected()
